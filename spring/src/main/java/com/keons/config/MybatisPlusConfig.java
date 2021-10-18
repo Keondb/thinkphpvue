961691c2d676a76873800eb677118b0350e0996e
starry <504750567@qq.com>
2021-10-19 06:49:28 +0800

    java

---
+++ spring/src/main/java/com/keons/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.keons.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Administrator
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.keons.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
